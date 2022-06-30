0f2f8c8ba8693ec26eb67156d43f158a4b4a9d20
Giovana <giomalaqui@gmail.com>
2022-06-29 23:33:42 -0300

    Padronização do Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { Router } from "./router/Router";
 import { GlobalStyle, Fundo } from "./Styled"
-import Header from "./components/Header";
-import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-
-      <Header />
       <Fundo>
         <Router />
       </Fundo>
